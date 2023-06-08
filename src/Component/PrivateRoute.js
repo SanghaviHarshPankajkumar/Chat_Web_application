26864fe5cb341b65cea47ea88581302fa6e52847
SanghaviHarshPankajkumar <84182620+SanghaviHarshPankajkumar@users.noreply.github.com>
2023-06-08 18:59:31 +0530

    Update PrivateRoute.js

--- src/Component/PrivateRoute.js
+++ src/Component/PrivateRoute.js
@@ -3,6 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
 const PrivateRoute = () => {
   const user = useContext(AuthContext);
-  return user ? <Outlet /> : <Navigate to="/Login" />;
+  return user.user ? <Navigate to="/Home" /> : <Navigate to="/Login" />;
 };
 export default PrivateRoute;
